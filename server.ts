cb023b6d33dd0b9ca0cc96a88ee80589b27cf45d
ssulochanaherath <ssulochanaherath.com>
2025-02-22 01:09:03 +0530

    Connected to the backend.

--- server.ts
+++ server.ts
@@ -1,19 +1,19 @@
 import express from "express";
-//import customerRoutes from "./routes/Customer-routes";
+import customerRoutes from "./routes/Customer-routes";
 
 const app = express();
-//var cors = require("cors")
+var cors = require("cors")
 
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 
 const corsOption={
     origin: 'http://localhost:5173',
     optionsSuccessStatus: 200
 }
-//app.use(cors(corsOption));
+app.use(cors(corsOption));
 
-//app.use("/Customer",customerRoutes);
+app.use("/Customer",customerRoutes);
 
 app.listen(3000,(err=>{
     console.log("server port 3000") ;
